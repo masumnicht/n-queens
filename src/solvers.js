d9987989145a3d6b1aaef33368e1e05465257504
masumnicht <masumnicht@gmail.com>
2015-10-12 20:05:19 -0700

    Working on find for queens

--- src/solvers.js
+++ src/solvers.js
@@ -13,8 +13,6 @@
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
-
-
 window.findNRooksSolution = function(n) {
   
   var b = new Board({n: n});
@@ -43,33 +41,13 @@ window.findNRooksSolution = function(n) {
   };
 
   inner(b);
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(b.rows()));
+  //console.log('Single solution for ' + n + ' rooks:', JSON.stringify(b.rows()));
   return b.rows();
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
 
-
-  // inner function takes in current board, row and col
-    // if row === length
-      // increment solution counter 
-
-    // if col === length
-      // return false
-
-    // toggle piece at this row, col
-
-    // check conflicts
-      // if none, call inner with board, row++, 0
-        // if inner returns false, untoggle, call inner with board, row, col++
-
-
-      // if conflicts, untoggle, call inner with board, row, col++  
-
-
-
-  
   var b = new Board({n: n});
   var count = 0;
 
@@ -105,25 +83,131 @@ window.countNRooksSolutions = function(n) {
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
   var b = new Board({n: n});
-  for (var i = 0; i < b.rows().length; i++){
-    for(var j = 0; j < b.rows()[i].length; j++) {
-      b.togglePiece(i,j);
-      if(b.hasRowConflictAt(i) || b.hasColConflictAt(j)|| b.hasAnyMajorDiagonalConflicts()||b.hasAnyMinorDiagonalConflicts()){
-        b.togglePiece(i,j);
-      }
+
+  var inner = function(board, row) {
+    row = row || 0;
+    if (row === board.rows().length) {
+      return true;
     }
 
-  }
-  
+    for (var i = 0; i < board.rows().length; i++) {
+      board.togglePiece(row, i);
+      if (board.hasAnyRowConflicts() || board.hasAnyColConflicts()|| board.hasAnyMajorDiagonalConflicts()||board.hasAnyMinorDiagonalConflicts()) {
+        board.togglePiece(row, i);
+        continue;
+      }
+      if (inner(board, row + 1)) {
+        
+        return true;
+      } else {
+        board.togglePiece(row, i);
+        continue; 
+      }
+    }
+    return false;
+  };
 
-  console.log('Single solution for ' + n + ' queens:', JSON.stringify(b.rows()));
+  inner(b);
+  //console.log('Single solution for ' + n + ' rooks:', JSON.stringify(b.rows()));
   return b.rows();
+  // var b = new Board({n: n});
+
+  // var inner = function(board, row, col) {
+    
+  //   console.log('inner called with:');
+  //   console.log(board.rows());
+  //   console.log('row:', row);
+  //   console.log('col:', col);
+
+  //   if (row === board.rows().length) {
+  //     console.log('bottom row reached');
+  //     console.log('returning true');
+  //     return true;
+  //   }
+    
+  //   if (col === board.rows().length) {
+  //     console.log('last col reached');
+  //     console.log('returning false');
+  //     return false;
+  //   }
+    
+  //   console.log('toggling piece...');
+  //   board.togglePiece(row, col);
+    
+  //   if (board.hasAnyRowConflicts() || board.hasAnyColConflicts()|| board.hasAnyMajorDiagonalConflicts()||board.hasAnyMinorDiagonalConflicts()) {
+  //     console.log('conflicts found, untoggling piece...');
+  //     board.togglePiece(row, col);
+  //     console.log('calling inner with row, col + 1');
+  //     inner(board, row, col + 1);
+  //   } else {
+  //     console.log('no conflicts found, calling inner with row + 1, col');
+  //     inner(board, row + 1, 0);
+  //     console.log('untoggling piece after calling inner with row + 1, col');
+  //     board.togglePiece(row, col);
+  //     console.log('calling inner with row, col + 1');
+  //     inner(board, row, col + 1);
+  //   }
+    
+  // };
+
+  // console.log('starting it off by calling inner with b');
+  // inner(b, 0, 0);
+  // //console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
+  // console.log('returning b.rows');
+  // return b.rows();
 };
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
-  var solution = undefined; //fixme
+  var b = new Board({n: n});
+  var count = 0;
 
-  console.log('Number of solutions for ' + n + ' queens:', solutionCount);
-  return solutionCount;
+  var inner = function(board, row, col) {
+    if (row === board.rows().length) {
+      count++;
+      return;
+    }
+    b.rows().forEach(function(el){
+      console.log(el);
+    })
+    
+    if (col === board.rows().length) {
+      return;
+    }
+
+    board.togglePiece(row, col);
+    
+    if (board.hasAnyRowConflicts() || board.hasAnyColConflicts()|| board.hasAnyMajorDiagonalConflicts()||board.hasAnyMinorDiagonalConflicts()) {
+      board.togglePiece(row, col);
+      inner(board, row, col + 1);
+    } else {
+      inner(board, row + 1, 0);
+      board.togglePiece(row, col);
+      inner(board, row, col + 1);
+    }
+
+  };
+
+  inner(b, 0, 0);
+  console.log('Number of solutions for ' + n + ' queens:', count);
+  return count;
+
+  // inner function takes in array, value, index
+  //   if index === arr.length
+  //     count++
+  //     return
+
+  //   if current value === arr.length
+  //     return
+
+  //   increase value at index by 1
+
+  //   if (conflicts)
+  //     decrease value
+  //     call inner with array, value++, index
+
+  //   if (no conflicts)
+  //     call inner with array, 0, index++
+  //     decrement value
+  //     call inner with array, value++, index
 };
